44ddf79acfd8bc6b9a2952b87e3ac15fc5e17f25
SeeWhysimon <appleforsimondick@icloud.com>
2024-12-06 22:38:34 +0800

    modified:   Array/CollatzConjecture.c

--- Array/CollatzConjecture.c
+++ Array/CollatzConjecture.c
@@ -1,30 +1,39 @@
 //P5727
 #include <stdio.h>
+#include <stdlib.h>
 
-#define MAX_LEN 100
+struct node;
+
+struct node {
+    int val;
+    struct node *prev;
+};
+
+struct node* makeNode(int val, struct node *prev) {
+    struct node *new_node = (struct node *)malloc(sizeof(struct node));
+    new_node->prev = prev;
+    new_node->val = val;
+    return new_node;
+}
 
 int main() {
     int n;
     scanf("%d", &n);
 
-    if (n == 1) {
-        printf("%d", n);
-        return 0;
-    }
+    struct node *head = makeNode(n, NULL);
+    struct node *curr_node, *prev_node;
+    curr_node = head;
     
-    int results[MAX_LEN];
-    int len = 0;
-
-    for (; n != 1; len++) {
-        results[len] = n;
-        if (n % 2 == 0) n /= 2;
-        else n = 3 * n + 1;
+    while (n != 1) {
+        if (n % 2) n = 3 * n + 1;
+        else n /= 2;
+        struct node *new_node = makeNode(n, curr_node);
+        curr_node = new_node;
     }
-    len++;
-    results[len] = 1;
 
-    for (int i = len - 1; i >= 0; i--) {
-        printf("%d ", results[i]);
+    while (curr_node) {
+        printf("%d ", curr_node->val);
+        curr_node = curr_node->prev;
     }
 
     return 0;
